6868961add9ceebbb2d0b6cb2c570d75c3b1ca52
finnchristoffer <finn.christoffer@gmail.com>
2023-01-13 01:19:36 +0700

    Fetching News Using RxWfit Extensions

--- NewsApp-MVVM/Controllers/NewsTableViewController.swift
+++ NewsApp-MVVM/Controllers/NewsTableViewController.swift
@@ -7,12 +7,28 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        populateNews()
+    }
+    
+    private func populateNews() {
+        
+        let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=id&apiKey=003f9cf2364c4f338df38fe6400114e7")!)
+        
+        URLRequest.load(resource: resource)
+            .subscribe(onNext: {
+                print($0)
+            }).disposed(by: disposeBag)
     }
 }
